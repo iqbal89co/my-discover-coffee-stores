30dabb57dd329fd5aaf6910e260d8699475ac71a
iqbal89co <iqbalfakhriza089@gmail.com>
2022-05-28 12:56:29 +0700

    add hello.js and footer in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      <footer><h1>Footer's right here</h1></footer>
+    </div>
+  )
 }
 
 export default MyApp
